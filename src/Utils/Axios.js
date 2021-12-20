063c99be997a2979791f607a26b4a6be7f8418d4
kylexrose@gmail.com <kylexrose@gmail.com>
2021-12-19 23:36:44 -0500

    baseURL

--- src/Utils/Axios.js
+++ src/Utils/Axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
 const Axios = axios.create({
-    baseURL: process.env.NODE_ENV === "development" 
-        ? "http://localhost:3001/api" 
-        : "/api",
+    baseURL: "/api",
     timeout: 50000,
     })
 
